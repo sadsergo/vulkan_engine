c89beef1c6c719a4de25d19651f21584b90630f4
sadjames <sergey.nikitin2013@gmail.com>
2024-11-08 16:41:42 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+int
+main()
+{
+    
+    return 0;
+}
