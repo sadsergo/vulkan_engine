5b4d78b3f3e93f978c43f8c1322db68756fba6d3
sadjames <sergey.nikitin2013@gmail.com>
2024-11-09 15:05:34 +0300

    write pipeline with vulkan

--- src/graphics_pipeline/pipeline.hpp
+++ src/graphics_pipeline/pipeline.hpp
@@ -8,7 +8,21 @@
 
 namespace lve
 {
-    struct PipelineConfigInfo {};
+    struct PipelineConfigInfo 
+    {
+        VkViewport viewport;
+        VkRect2D scissor;
+        VkPipelineViewportStateCreateInfo viewportInfo;
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo multisampleInfo;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+        VkPipelineLayout pipelineLayout = nullptr;
+        VkRenderPass renderPass = nullptr;
+        uint32_t subpass = 0;
+    };
 
     class Pipeline
     {
@@ -18,7 +32,7 @@ namespace lve
             const std::string &vertFilePath, 
             const std::string &fragFilePath, 
             const PipelineConfigInfo &configInfo);
-        ~Pipeline() {}
+        ~Pipeline();
 
         Pipeline(const Pipeline &) = delete;
         void operator=(const Pipeline &) = delete;
