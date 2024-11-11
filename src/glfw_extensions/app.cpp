47e3139e23a87ecef6f53a8a2fd419e158f1acdf
Sergei Nikitin <sergey.nikitin2013@gmail.com>
2024-11-11 17:34:22 +0300

    add 3D objs load to Vulkan

--- src/glfw_extensions/app.cpp
+++ src/glfw_extensions/app.cpp
@@ -46,22 +46,66 @@ namespace lve {
     vkDeviceWaitIdle(device.device());
   }
 
-  void App::loadGameObjects() {
+  std::unique_ptr<Model> createCubeModel(LveDevice& device, glm::vec3 offset) {
     std::vector<Model::Vertex> vertices {
-        {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-        {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-        {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
+    // left face (white)
+    {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+    {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+    {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+    {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+    {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+    {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+    // right face (yellow)
+    {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+    {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+    {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+    {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+    {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+    {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+    // top face (orange, remember y axis points down)
+    {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    // bottom face (red)
+    {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    // nose face (blue)
+    {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    // tail face (green)
+    {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    };
 
-    auto model = std::make_shared<Model>(device, vertices);
+    for (auto& v : vertices) {
+      v.position += offset;
+    }
     
-    auto triangle = GameObject::createGameObject();
-    triangle.model = model;
-    triangle.color = {.1f, .8f, .1f};
-    triangle.transform2D.translation.x = .2f;
-    triangle.transform2D.scale = {2.f, .5f};
-    triangle.transform2D.rotation = .25f * glm::two_pi<float>();
+    return std::make_unique<Model>(device, vertices);
+  }
 
-    gameObjects.push_back(std::move(triangle));
+  void App::loadGameObjects() {
+      std::shared_ptr<Model> lveModel = createCubeModel(device, {.0f, .0f, .0f});
+      auto cube = GameObject::createGameObject();
+      cube.model = lveModel;
+      cube.transform.translation = {.0f, .0f, .5f};
+      cube.transform.scale = {.5f, .5f, .5f};
+      gameObjects.push_back(std::move(cube));
   }
 
 };
--- src/renderer/render_system.cpp
+++ src/renderer/render_system.cpp
@@ -14,9 +14,8 @@ namespace lve {
 
   struct SimplePushConstantData
   {
-    glm::mat2 transform {1.f};
-    glm::vec2 offset;
-    alignas(16) glm::vec3 color;
+    glm::mat4 transform {1.f};
+    alignas(16) glm::vec3 color {};
   };
 
   RenderSystem::RenderSystem(LveDevice& device, VkRenderPass renderPass) : device {device}
@@ -66,11 +65,12 @@ namespace lve {
 
     for (auto &obj: gameObjects)
     {
-      obj.transform2D.rotation = glm::mod(obj.transform2D.rotation + 0.01, glm::two_pi<double>());
+      obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
+      obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.005f, glm::two_pi<float>());
+      
       SimplePushConstantData push {};
-      push.offset = obj.transform2D.translation;
       push.color = obj.color;
-      push.transform = obj.transform2D.mat2();
+      push.transform = obj.transform.mat4();
 
       vkCmdPushConstants(
         commandBuffer, 
